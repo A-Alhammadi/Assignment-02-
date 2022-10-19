2da408c1060fafb16d47d2ab7379890e790837eb
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2022-10-18 19:57:11 -0500

    Update source.cpp

--- Assignment 2 Main/source.cpp
+++ Assignment 2 Main/source.cpp
@@ -15,29 +15,29 @@ int main() {
 	double h;
 	int type; 
 	
-	Professional prof;
-	Nonprofessional non;
-	Employee* professional = &prof;
-	Employee* nonprofessional = &non;
+	Professional prof;//object for professional employee class
+	Nonprofessional non;//object for nonprofessional employee class
+	Employee* professional = &prof; // pointer to use derived class for professional
+	Employee* nonprofessional = &non;// pointer to use derived class for nonprofessional
 
 	cout << " Enter the employee type: enter 1 for professional or 2 for nonprofessional: " << endl;
 	cin >> type;
-	while (type < 1 || type > 2) {
+	while (type < 1 || type > 2) { //user inputs 1 or 2. If any othher input is entered, they will be asked again
 		cout << " Please enter either 1 for professional or 2 for nonprofessional: " << endl;
 		cin >> type;
 	}
-	while (type == 1 || type == 2) {
+	while (type == 1 || type == 2) {//while  type is either 1 or 2, the switch statement below will be called
 		switch (type) {
-		case 1:
+		case 1: //this is for the professional option
 		{
 			cout << endl << " You have chosen professional employee, please enter the required information for this employee. " 
 				<< endl << " Enter the monthly salary of this professional employee: " << endl;
-			cin >> m;
-			double sp = professional->calcSalary(m);
-			professional->setSal(sp);
+			cin >> m; //this would be the monthly salary
+			double sp = professional->calcSalary(m);//this calls the calcSalary function and sets the double dp to whatever the function calculates
+			professional->setSal(sp); //set function to set private variable
 
 			cout << endl << " Enter the number of vacation days for this employee: " << endl;
-			cin >> d;
+			cin >> d;//this is the number of vacation days...
 			int vp = professional->vacationDays(d);
 			professional->setVac(vp);
 
@@ -47,16 +47,16 @@ int main() {
 			professional->setIns(ip);
 
 			cout << endl << " This employee's information was enetered as follows: " << endl << " Monthly Salary : " << m << endl
-				<< " Yearly Paid Vacation days: " << d << endl << " Yearly Health Insurance: " << h << endl;
+				<< " Yearly Paid Vacation days: " << d << endl << " Yearly Health Insurance: " << h << endl;//prints information for the employee
 			break;
 			
 		}
 
-		case 2:
+		case 2: //this is for the nonprofessional option
 		{
 			cout << endl << " You have chosen Nonprofessional employee, please enter the number of hours the employee works in a week: " << endl;
 			cin >> hours;
-			double sn = nonprofessional->calcSalary(hours);
+			double sn = nonprofessional->calcSalary(hours);//same idea as the above functions
 			nonprofessional->setSal(sn);
 
 			int vn = nonprofessional->vacationDays(hours);
